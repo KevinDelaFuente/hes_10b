1849cf487935ac22f32759f30bc9c0e55a56bc9b
Kevin De la Fuente <kevin.delafuenteg@gmail.com>
2026-02-10 22:23:16 -0800

    Update Palindrome.java

--- pset1/Palindrome.java
+++ pset1/Palindrome.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 /**
  * Palindrome.java
  * 
- * A program that determines whether a user-inputted string is a palindrome.
- * Ignores spaces, punctuation, and case differences.
- * Test cases commented out below.
+ * A program that determines whether a user-inputted string is a palindrome using recursive logic.
  */
 class Palindrome {
     
     public static void main (String [] args) {
+        // Test Cases:
         // String test1 = "racecar";
         // String test2 = "hello";
         // System.out.println (test1 + " is palindrome? " + isPalindrome(test1));
@@ -23,17 +22,15 @@ class Palindrome {
         }
     }
 
-    /**
-     * Recursively determines if a string is a palindrome.
-     * Skips non-alphanumeric characters (spaces, punctuation).
-     * Assumes input has already been converted to lowercase.
-     */
+    //  Recursively determines if a string is a palindrome.
+    //  Skips non-alphanumeric characters (spaces, punctuation) using Character.isLetterOrDigit().
+    //  assumes input has already been converted to lowercase in main or userInput.
     public static boolean isPalindrome (String s){
         int s_length = s.length();
         int left_index = 0;
         int right_index = s_length - 1;
 
-        // Base case: empty string or single character is a palindrome
+        // In case of empty string or single character, defined as palindrome
         if (s_length <= 1)
             return true;
         else {
@@ -41,16 +38,17 @@ class Palindrome {
             while (left_index < s_length && !Character.isLetterOrDigit(s.charAt(left_index))) {
                 left_index++;
             }
-            // Skip non-alphanumeric characters from right
+            // Same logic but in reverse
             while (right_index >= 0 && !Character.isLetterOrDigit(s.charAt(right_index))) {
                 right_index--;
             }
-            // If pointers crossed or met and it hasn't stopped before it will be a palindrome. I thought it would be part of the base case
+            // If pointers crossed or met and it hasn't stopped before, it will be a palindrome. I thought it would be part of the base case
             // but sometimes I get substrings that are ' b' for example that are lenght 2 that don't fall in the base case but only have 
             // one letter to compare
             if (left_index >= right_index) {
                 return true;
             }
+            // Debugging statements:
             // System.out.println("Comparing " + s_lower.charAt(left_index) + " and " + s_lower.charAt(right_index));
             // System.out.println("Substring is: " + s.substring(left_index+1,right_index) + ", length: " + (right_index - left_index - 1));
             return (s.charAt(left_index) == (s.charAt(right_index)) && isPalindrome(s.substring(left_index+1,right_index)));
